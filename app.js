98bf78eb91422491542c9c3b54aebf8fa4a141eb
Mathieu <mathieu.compagnon@esme.fr>
2017-01-03 00:11:06 +0100

    use npm to generate package.json

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {	
-	
-	
-	var page = url.parse(req.url).pathname;
-
 		
+	var page = url.parse(req.url).pathname;
+	var params = querystring.parse(url.parse(req.url).query);
+
 	console.log("HELLO WORLD NODE JS !!");	
 	  
 
 	res.writeHead(200, {'content-type': 'text/javascript'});  
-
-
 	res.end("Test !");		
-		  
+	
 });
 
 server.listen(80);
