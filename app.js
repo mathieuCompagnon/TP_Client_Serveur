31e85a7ba09d25a9b71372faac8a915ea70dc277
Mathieu <mathieu.compagnon@esme.fr>
2017-01-03 23:46:09 +0100

    start parsing url path and include google distance

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
+var distance = require('google-distance');
+ 
+ 
+ var car = [];
+ 
  
 var server = http.createServer(function(req, res) {	
 		
-	var page = url.parse(req.url).pathname;
+	var path = url.parse(req.url).pathname;
 	var params = querystring.parse(url.parse(req.url).query);
 
-	console.log("HELLO WORLD NODE JS !!");	
+	console.log("HELLO WORLD NODE JS !!");		  
+	res.writeHead(200, {'Content-Type': 'application/json'});  	
+	var to_send;  
 	  
-
-	res.writeHead(200, {'content-type': 'text/javascript'});  
-	res.end("Test !");		
+	if(path == '/connect'){
+		
+	}else if(path == ''){
+		
+	}else if(path == ''){
+		
+	}else if(path == ''){
+		
+	}else if(path == ''){
+		
+	}else if(path == ''){
+		
+	}else if(path == ''){
+		
+	}	  	  
+	
+	res.end(JSON.stringify(to_send));		
 	
 });
 
