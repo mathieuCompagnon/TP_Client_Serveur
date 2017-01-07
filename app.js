743a9e873471582980c056c8a2a845231ad4df77
Mathieu <mathieu.compagnon@esme.fr>
2017-01-07 23:19:28 +0100

    Add xo to package.json

--- app.js
+++ app.js
@@ -11,18 +11,30 @@ var server = http.createServer(function(req, res) {
 		
 	var path = url.parse(req.url).pathname;
 	var params = querystring.parse(url.parse(req.url).query);
+	
+    //Cross origine request error
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 
-	console.log("HELLO WORLD NODE JS !!");		  
+	console.log(path);		  
 	res.writeHead(200, {'Content-Type': 'application/json'});  	
 	var to_send;  
 	  
 	if(path == '/connect'){
 		
-	}else if(path == ''){
+	}else if(path == '/getVoiture' && params['id'] ){		
+		if(car[params['id']]){
+			console.log("yep");
+			to_send = {voitureData : car[params['id']]};
+		}else{
+			console.log("nop");
+			to_send = {error : 'not found'};
+		}
+	}else if(path == '/setVoiture'){
 		
-	}else if(path == ''){
-		
-	}else if(path == ''){
+	}else if(path == '/delVoiture'){
 		
 	}else if(path == ''){
 		
