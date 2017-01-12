dd9e70ea2a94307502b9a0ed4d2e334cc4f10229
mathieu <mathieu.compagnon@esme.fr>
2017-01-12 16:44:56 +0100

    try using express

--- app/app.js
+++ app/app.js
@@ -1,24 +1,31 @@
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
-var distance = require('google-distance');
+
+var express = require('express');
+var app = express();
+
+var vehicule = [];//require('./model/vehicule.js');
+var user = require('./model/user.js'); 
  
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/TDvehicule');
  
- var car = [];
- 
- 
-var server = http.createServer(function(req, res) {	
-		
-	var path = url.parse(req.url).pathname;
-	var params = querystring.parse(url.parse(req.url).query);
-	
-    //Cross origine request error
-	res.setHeader('Access-Control-Allow-Origin', '*'); 
+  
+
+app.use(function (req, res, next) {	
+
+	//Cross origine request error
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 
-	console.log(path);		  
+	var path = url.parse(req.url).pathname;
+	var params = querystring.parse(url.parse(req.url).query);
+	console.log(path);	
+	
 	res.writeHead(200, {'Content-Type': 'application/json'});  	
 	var to_send;  
 	  
@@ -44,8 +51,9 @@ var server = http.createServer(function(req, res) {
 		
 	}	  	  
 	
-	res.end(JSON.stringify(to_send));		
-	
+	res.end(JSON.stringify(to_send));
+	next();
 });
-
-server.listen(80);
+ 
+ 
+app.listen(80);
